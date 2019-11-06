8e0a6cba58b7c3f6ea6fce8c170e1fdd25a71536
MichaelRol <michael.rollins@hotmail.co.uk>
2019-11-06 16:59:55 +0000

    Close all threads when nonce is found

--- parallel_pow.py
+++ parallel_pow.py
@@ -3,7 +3,7 @@ import sys
 import time
 import multiprocessing
 
-def find_golden_nonce(d, process):
+def find_golden_nonce(d, process, event):
     block = "COMSM0010cloud"
     block = bin(int.from_bytes(block.encode(), 'big'))
     binaryblock = block.replace("b", "")
@@ -23,23 +23,36 @@ def find_golden_nonce(d, process):
         leadingz = 256-len(str(bin(int(hashsq, 16))[2:]))
         if leadingz >= d:
             end = time.time()
+            event.set()
             print("Process: " + str(process) + ", Nonce: " + str(nonce) + ", Time: " + str(end - start))
             return
     print("No golden nonce found.")
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Please run python3 serial_pow.py <difficulty level>")
-else:
-    try:
-        difficulty = int(sys.argv[1])
-        if difficulty > 256:
-            print("Difficulty value too large.")
-            raise ValueError()
-        for index in range(0, 4):
-            p = multiprocessing.Process(target=find_golden_nonce, args=(difficulty, index,))
-            p.start()
-    except ValueError:
-        print("Please enter a integer value below 256 when running the program.")
+if __name__ == '__main__':
     
+    jobs = []
+    event = multiprocessing.Event()
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
+        print("Please run python3 serial_pow.py <difficulty level>")
+    else:
+        try:
+            difficulty = int(sys.argv[1])
+            if difficulty > 256:
+                print("Difficulty value too large.")
+                raise ValueError()
+            for index in range(0, 4):
+                p = multiprocessing.Process(target=find_golden_nonce, args=(difficulty, index, event,))
+                jobs.append(p)
+                p.start()
+        except ValueError:
+            print("Please enter a integer value below 256 when running the program.")
+        
+        while True:
+            if event.is_set():
+                for i in jobs:
+                    i.terminate()
+                sys.exit(1)
+            time.sleep(1)
+        
 
 
