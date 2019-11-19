b11d868fd8e4d65a8037234c7f6b19c754cdfedd
MichaelRol <michael.rollins@hotmail.co.uk>
2019-11-19 23:27:27 +0000

    Now using pxssh to interact with VM

--- cloud_pow.py
+++ cloud_pow.py
@@ -1,4 +1,4 @@
-import pexpect 
+from pexpect import pxssh
 import sys
 import boto3
 
@@ -6,10 +6,8 @@ if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Yah dun it wrong")
 else:
     difficulty = int(sys.argv[1])
-    child = pexpect.spawn("/bin/bash")
-    child.sendline("ssh -i ~/.ssh/CloudCOmp.pem ubuntu@ec2-3-8-48-186.eu-west-2.compute.amazonaws.com")
-    child.expect("Last login")
-    child.readline()
+    child = pxssh.pxssh(timeout=1000, options={"StrictHostKeyChecking": "no"})
+    child.login("ec2-3-10-242-69.eu-west-2.compute.amazonaws.com", username="ubuntu", ssh_key="~/.ssh/CloudCOmp.pem")
     command = "python3 steps_pow.py " + str(difficulty) + " 10"
     child.sendline(command)
     child.readline()
