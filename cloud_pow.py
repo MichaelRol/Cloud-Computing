d21f0329082b0047db57e6f25727370df858820e
MichaelRol <michael.rollins@hotmail.co.uk>
2019-11-21 17:28:50 +0000

    Creates ec2 instance

--- cloud_pow.py
+++ cloud_pow.py
@@ -18,12 +18,22 @@ key_pair_to_write = str(key_pair.key_material)
 outfile.write(key_pair_to_write)
 pexpect.run("chmod 400 ec2-keypair.pem")
 
+instances = ec2.create_instances(
+     ImageId='ami-0be057a22c63962cb',
+     MinCount=1,
+     MaxCount=1,
+     InstanceType='t2.micro',
+     KeyName='ec2-keypair'
+ )
+
+print(str(instances))
+
 if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Yah dun it wrong")
 else:
     difficulty = int(sys.argv[1])
     child = pxssh.pxssh(timeout=1000, options={"StrictHostKeyChecking": "no"})
-    child.login("ec2-3-10-242-69.eu-west-2.compute.amazonaws.com", username="ubuntu", ssh_key="~/.ssh/CloudCOmp.pem")
+    child.login("ec2-3-10-242-69.eu-west-2.compute.amazonaws.com", username="ubuntu", ssh_key="~/.ssh/ec2-keypair.pem")
     command = "python3 steps_pow.py " + str(difficulty) + " 10"
     child.sendline(command)
     child.readline()
